ce42bb92819388af8177d5082a6d22e1b425425f
kemi <brandonduongneon@gmail.com>
2021-08-04 19:22:09 -0700

    renamed create_credential

--- Encryption/Shutdown_Pc.ps1
+++ Encryption/Shutdown_Pc.ps1
@@ -1,7 +1,9 @@
-$User = 'brandonduongneon@outlook.com'
+$User = Read-Host 'Please input your username: '
 $PasswordFile = 'encrypted.test'
 $KeyFile = 'key.test'
+$PCName = 'BRANDON-LAPTOP'
 $key = Get-Content $KeyFile
 $MyCredential = New-Object -TypeName System.Management.Automation.PSCredential `
  -ArgumentList $User, (Get-Content $PasswordFile | ConvertTo-SecureString -Key $key)
-Invoke-Command -ComputerName BRANDON-LAPTOP -ScriptBlock {Restart-Computer -Force} -Credential $MyCredential
+
+Invoke-Command -ComputerName $PCName -ScriptBlock {Restart-Computer -Force} -Credential $MyCredential
